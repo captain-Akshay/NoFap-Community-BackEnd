40d790311bfd286604b5c3e3975acf2feb9189b4
Akshay Sharma <59491379+captain-Akshay@users.noreply.github.com>
2023-03-30 21:07:24 +0530

    fetch was missing

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
+import fetch from "node-fetch";
 import path from "path";
 import { fileURLToPath } from "url";
 import {initializeApp} from"firebase/app";
